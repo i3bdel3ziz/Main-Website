bedef8c55b10586a8ccdcd2ce3822c7e9f41828a
Abdulaziz Jamal <ajamal444@live.com>
2016-08-04 09:31:32 +0300

    added form.php file.

--- Website/pages/form.php
+++ Website/pages/form.php
@@ -1,4 +1,14 @@
 <?php
+if ( isset($_POST['email']) && isset($_POST['name']) && isset($_POST['message']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
 
+  
+  $test = "/(content-type|bcc:|cc:|to:)/i";
+  foreach ( $_POST as $key => $val ) {
+    if ( preg_match( $test, $val ) ) {
+      exit;
+    }
+  }
 
-?>
+  //
+  mail( "", AAJ , $_POST['message'], "From:" . $_POST['email'] );}
+?
